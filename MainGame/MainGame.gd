4b491be343dfc9723bca88299507d2b04af98c19
novastick <99231858+novastick@users.noreply.github.com>
2022-02-28 09:50:51 +1100

    updates form the project
    the updates that where done to the code

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,14 +1,23 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export(int) var countdownmax
+var currentTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	currentTimer = countdownmax
+	$HUD/Countdown.text = str(currentTimer)
+	
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("gameover")
+	
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
