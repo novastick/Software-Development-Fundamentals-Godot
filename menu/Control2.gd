5054a548c0f17b31d70a81076fc9917489402bcc
novastick <99231858+novastick@users.noreply.github.com>
2022-04-05 15:08:33 +1000

    update to code
    try and get this to personal computer

--- menu/Control2.gd
+++ menu/Control2.gd
@@ -1,15 +1,13 @@
 extends Control
 
-
-
-
 func _ready():
-	for button in $"layout/main/buttons".get_children():
+	for button in $"menu/menubuttons/buttons".get_children():
 		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
-		
-		
 
 
-func _on_Button_Pressed(scene_to_load):
+
+
+func _on_button_pressed(scene_to_load):
+	print("Changing Scene...")
 	print(scene_to_load)
 	get_tree().change_scene(scene_to_load)
